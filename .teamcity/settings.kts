29276b69714178aa2d38e506ac470c70936658e6
SergeyKiselev1995 <s.kiselyov@itransition.com>
2019-07-12 15:46:39 +0300

    Fix imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 /*
